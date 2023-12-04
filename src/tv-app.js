b758ae32dc391382d4a06142d1c705e701711ef2
Rsciote <143344057+Rsciote@users.noreply.github.com>
2023-12-03 23:04:56 -0500

    next check

--- src/tv-app.js
+++ src/tv-app.js
@@ -6,7 +6,7 @@ import "@lrnwebcomponents/video-player/video-player.js";
 import "./tv-channel.js";
 
 
-const data = "../assets/channels.json"
+
 export class TvApp extends LitElement {
   // defaults
   constructor() {
@@ -14,7 +14,14 @@ export class TvApp extends LitElement {
     this.name = '';
     this.source = new URL('../assets/channels.json', import.meta.url).href;
     this.listings = [];
+    this.sourceVideo = "https://www.youtube.com/watch?v=rrc0Y3JMk6Q";
+    this.currentInfo={
+      title: null,
+      timecode: null,
+      description: null,
+    }
   }
+
   // convention I enjoy using to define the tag's name
   static get tag() {
     return 'tv-app';
@@ -25,6 +32,7 @@ export class TvApp extends LitElement {
       name: { type: String },
       source: { type: String },
       listings: { type: Array },
+      currentInfo:{type: Object}
     };
   }
   // LitElement convention for applying styles JUST to our element
@@ -51,6 +59,10 @@ export class TvApp extends LitElement {
           border: 4px solid rgba(0, 0, 0, 0.8);
           background-color: grey;
       }
+      .videoScreen{
+        height: 357px;
+          width: 700px; 
+      } 
       .grid-item2
         {
           height: 700px;
@@ -58,6 +70,28 @@ export class TvApp extends LitElement {
           border: 1px solid rgba(0, 0, 0, 0.8);
           background-color: beige;
         }
+        .scroll-container 
+        {
+          width: 350px;
+          height: 800px;
+          overflow-y: auto;
+          padding-top: 5px;
+          padding-left: 25px;
+        }
+        .descriptionSlides
+        {
+          width: 700px;
+          height: 250px;
+          background-color: black;
+          border:  1px solid red;
+        }
+        .descriptionContent
+        {
+          font-size: 16px;
+          color: white;
+
+        }
+        
       `
     ];
   }
@@ -70,9 +104,44 @@ export class TvApp extends LitElement {
     <div class="grid-item2">
     </div>
       </div>
+      <div class="descriptionSlides">
+           <h3 class="descriptionContent"> "${this.currentInfo}"</h3>
+      </div>
+        </div>
+      <div class="grid-item2">
+        <div class="scroll-container"> 
+        
+        
+
     `
     }
      
+    currentItem(e) {
+    
+      console.log(e.target);
+      this.shadowRoot.querySelector('video-player').shadowRoot.querySelector('a11y-media-player').play();
+      this.shadowRoot.querySelector('video-player').shadowRoot.querySelector('a11y-media-player').seek(e.target.timecode);
+    }
+     descriptionInfo()
+     {
+  
+     }
+    currentVidTime()
+    {
+        const timeNow = this.shadowRoot.querySelector('video-player').shadowRoot.querySelector("a11y-media-player").media.currentTime;
+    
+        this.listings.forEach((list) => {
+          const currentTimecode = list.timecode;
+    
+          // Check if the absolute difference is within the threshold
+          if (Math.abs(timeNow - currentTimecode) < this.timeThreshold) {
+            this.currentInfo = list.description;
+            return;
+          }
+        });
+  }
+
+
   // LitElement life cycle for when any property changes
   updated(changedProperties) {
     if (super.updated) {
@@ -102,5 +171,6 @@ export class TvApp extends LitElement {
     
   }
 }
+
 // tell the browser about our tag and class it should run when it sees it
 customElements.define(TvApp.tag, TvApp);
